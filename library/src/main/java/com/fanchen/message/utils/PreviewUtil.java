6c8570abef55a2bb8129568d8bc0f0e1fb38caa7
fanchen <happy1993_chen@sina.cn>
2020-04-26 14:02:47 +0800

    fix bug

--- library/src/main/java/com/fanchen/message/utils/PreviewUtil.java
+++ library/src/main/java/com/fanchen/message/utils/PreviewUtil.java
@@ -4,6 +4,7 @@ import android.content.Context;
 
 import com.fanchen.message.commons.models.IMessage;
 import com.fanchen.picture.ImagePreview;
+import com.fanchen.picture.bean.IImageInfo;
 import com.fanchen.picture.view.listener.OnPopItemClickListener;
 
 import java.util.ArrayList;
@@ -21,6 +22,16 @@ public class PreviewUtil {
         instance.setEnableDragClose(true);
         instance.setShowDownButton(true);
         instance.setIndex(position).setImageList(strings).start();
+    }
+
+    public static void startInfoPreview(Context context, List<? extends IImageInfo> list, int position, OnPopItemClickListener clickListener) {
+        ImagePreview instance = ImagePreview.getInstance();
+        instance.setContext(context);
+        instance.setOnPopItemClickListener(clickListener);
+        instance.setShowCloseButton(true);
+        instance.setEnableDragClose(true);
+        instance.setShowDownButton(true);
+        instance.setIndex(position).setImageInfoList(list).start();
     }
 
 }
